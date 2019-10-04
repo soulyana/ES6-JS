c7cd378e544facc3859e45abf6c3ae4a1a93def5
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-04 14:16:04 -0400

    Maps

--- script.js
+++ script.js
@@ -397,7 +397,7 @@ console.log(ages.find(cur => cur >= 18));
    console.log(john);
    var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish');
    console.log(emily);
-   */
+   
 
 //ES6
 function SmithPerson(
@@ -416,3 +416,45 @@ var john = new SmithPerson("John", 1990);
 console.log(john);
 var emily = new SmithPerson("Emily", 1983, "Diaz", "Spanish");
 console.log(emily);
+*/
+
+/**
+ * Maps 
+ */
+
+ const question = new Map();
+ question.set('question', 'What is the official name of the latest major Javascript version?');
+ question.set(1, 'ES5');
+ question.set(2, 'ES6');
+ question.set(3, 'ES2015');
+ question.set(4, 'ES7');
+ question.set('correct', 3);
+ question.set(true, 'Correct answer :D');
+ question.set(false, 'Wrong, please try again');
+
+ console.log(question.get('question'));
+//  console.log(question.size);
+
+ if (question.has(4)) {
+     //question.delete(4);
+    //  console.log('Answer 4 is here');
+ }
+ 
+ //question.clear();
+
+ //question.forEach((value, key) => console.log(`This is ${key}, and it's set to ${value}`));
+
+ for (let [key, value] of question.entries()) {
+    //console.log(`This is ${key}, and it's set to ${value}`);
+    if(typeof(key) === 'number') {
+        console.log(`Answer ${key}: ${value}`);
+    }
+ }
+
+ const ans = parseInt(prompt('Write the correct answer'));
+console.log(question.get(ans === question.get('correct')));
+
+// Maps allow us to use anything as keys
+// Maps are iterable 
+// Maps have size property
+// Maps can easily add and remove a property
