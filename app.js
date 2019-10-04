4f0f75b867f4ce0ced9bb06ba7e715f1ec928eb6
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-03 21:54:01 -0400

    Arrow Functions: Basics

--- app.js
+++ app.js
@@ -80,6 +80,7 @@ console.log(c);
 /**
  * Strings in ES6
  */
+/*
 
  let firstName = 'John';
  let lastName = 'smith';
@@ -100,3 +101,31 @@ console.log(c);
  console.log(n.endsWith('th'));
  console.log(n.includes('oh'));
  console.log(`${firstName} `.repeat(5));
+ */
+
+ /**
+  * Arrow Functions: Basics
+  */
+
+  const years = [1990, 1965, 1982, 1937];
+
+  // ES5
+  var ages5 = years.map(function(el) {
+      return 2019 - el;
+  });
+  console.log(ages5);
+
+  // ES6
+  let ages6 = years.map(el => 2019 - el);
+  console.log(ages6);
+
+  ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+  console.log(ages6);
+
+  ages7 = years.map((el, index) => {
+      const now = new Date().getFullYear();
+      const age = now - el;
+      return `Age element ${index + 1}: ${age}.`
+  });
+  console.log(ages7);
+
