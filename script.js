3149ab644ee0d6dd1ea1617efe97bc36b22c5c5d
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-04 12:33:43 -0400

    The Spread Operator

--- script.js
+++ script.js
@@ -283,7 +283,7 @@ for (const cur of boxesArr6) {
     }
     cur.textContent = 'I changed to blue!';
 } 
-*/
+
 
 // ES5
 var ages = [12, 17, 8, 21, 14, 11];
@@ -299,3 +299,35 @@ console.log(ages[full.indexOf(true)]);
 // ES6
 console.log(ages.findIndex(cur => cur >= 18));
 console.log(ages.find(cur => cur >= 18));
+*/
+
+/**
+ * The Spread Operator
+ */
+
+ function addFourAges (a, b, c, d) {
+     return a + b + c + d;
+ }
+ var sum1 = addFourAges(18, 38, 12, 21);
+ console.log(sum1);
+
+ // ES5
+ var ages = [18, 38, 12, 21];
+ var sum2 = addFourAges.apply(null, ages);
+ console.log(sum2);
+
+ //ES6
+ //expands array into its components
+ const sum3 = addFourAges(...ages); 
+ console.log(sum3);
+
+ const familySmith = ['John', 'Jane', 'Mark'];
+ const familyMiller = ['Mary', 'Bob', 'Ann'];
+ const bigFamily = [...familySmith, 'Lily', ...familyMiller];
+ console.log(bigFamily);
+
+ const h = document.querySelector('h1');
+ const boxes = document.querySelectorAll('.box');
+ const all = [h, ...boxes];
+
+ Array.from(all).forEach(curr => curr.style.color = 'purple');
