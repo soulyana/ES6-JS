39b7b71cea649af5db96d5f9c88b425ec6a8bb6a
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-04 11:53:00 -0400

    Arrays in ES6 / ES2015

--- script.js
+++ script.js
@@ -210,11 +210,12 @@ console.log(c);
  /**
   * Destructuring
   */
+ /*
 
   // ES5
   var john = ['John', 26];
-//   var name = john[0];
-//   var age = john[1];
+    //   var name = john[0];
+    //   var age = john[1];
 
   // ES6 
   const [name, age] = ['John', 26]
@@ -242,3 +243,59 @@ console.log(c);
   const [age2, retirement] = calcAgeRetirement(1990);
   console.log(age2);
   console.log(retirement);
+*/
+
+/**
+ * Arrays in ES6 / ES2015
+ */
+
+/*
+ const boxes = document.querySelectorAll('.box');
+
+ // ES5
+ var boxesArr5 = Array.prototype.slice.call(boxes);
+ boxesArr5.forEach(function(cur) {
+     cur.style.backgroundColor = 'dodgerblue';
+ });
+
+ // ES6
+ const boxesArr6 = Array.from(boxes);
+Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+
+// ES5
+for(var i = 0; i < boxesArr5.length; i++) {
+
+    if (boxesArr5[i].className === 'box blue') {
+        continue;
+    }
+
+    boxesArr5[i].textContent = 'I changed to blue!';
+}
+
+
+
+
+// ES6 - forOf loop
+for (const cur of boxesArr6) {
+    if (cur.className.includes('blue')) {
+       continue;
+    }
+    cur.textContent = 'I changed to blue!';
+} 
+*/
+
+// ES5
+var ages = [12, 17, 8, 21, 14, 11];
+
+var full = ages.map(function(cur) {
+    return cur >= 18;
+});
+console.log(full);
+
+console.log(full.indexOf(true));
+console.log(ages[full.indexOf(true)]);
+
+// ES6
+console.log(ages.findIndex(cur => cur >= 18));
+console.log(ages.find(cur => cur >= 18));
