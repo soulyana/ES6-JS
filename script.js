87a790695949c1d6269a47fab7d2cba34bd94e0e
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-04 13:41:29 -0400

    Rest Parameters

--- script.js
+++ script.js
@@ -304,6 +304,7 @@ console.log(ages.find(cur => cur >= 18));
 /**
  * The Spread Operator
  */
+/*
 
  function addFourAges (a, b, c, d) {
      return a + b + c + d;
@@ -331,3 +332,47 @@ console.log(ages.find(cur => cur >= 18));
  const all = [h, ...boxes];
 
  Array.from(all).forEach(curr => curr.style.color = 'purple');
+ */
+
+ /**
+  * Rest Parameters
+  */
+/*
+  // ES5
+  function isFullAge5() {
+      //console.log(arguments);
+      var argsArr = Array.prototype.slice.call(arguments);
+
+      argsArr.forEach(function(cur) {
+          console.log((2016 - cur) >= 18);
+      });
+  }
+
+  // isFullAge5(1990, 1999, 1965);
+  //isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+  // ES6
+  function isFullAge6(...years) {
+    years.forEach(cur => console.log((2016 - cur) >= 18));
+  }
+  isFullAge5(1990, 1999, 1965, 2016, 1987);
+  */
+
+  // ES5
+  function isFullAge5(limit) {
+      //console.log(arguments);
+      var argsArr = Array.prototype.slice.call(arguments, 1);
+
+      argsArr.forEach(function(cur) {
+          console.log((2016 - cur) >= 18);
+      });
+  }
+
+  //isFullAge5(16, 1990, 1999, 1965);
+  //isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+  // ES6
+  function isFullAge6(limit, ...years) {
+    years.forEach(cur => console.log((2016 - cur) >= limit));
+  }
+  isFullAge5(16, 1990, 1999, 1965, 2016, 1987);
